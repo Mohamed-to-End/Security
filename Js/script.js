714c209eb105bef9bc79c2c5817a259f275662a5
Mohamed to End <moezzat12345amr@gmail.com>
2025-01-04 19:29:51 +0200

    Button up

--- Js/script.js
+++ Js/script.js
@@ -2,3 +2,16 @@ AOS.init({
     duration: 3000,
     once: true,
   });
+  let y = window.scrollY
+  let buttonup = document.getElementsByClassName("buttonup")[0];
+  window.addEventListener("scroll",() =>{
+    if (window.scrollY > 0) {
+      buttonup.style.display = "block";
+    } else {
+      buttonup.style.display = "none";
+    }
+    y = window.scrollY
+  })
+  buttonup.addEventListener("click",() => {
+    window.scrollTo(0,0)
+  })
